005aabedb3e176376aadab8c4eb6321470837149
user <user@example.com>
2025-09-24 12:18:18 +0200

    improve display_spectra

--- display_spectra.py
+++ display_spectra.py
@@ -1,37 +1,33 @@
-import os
-import shutil
-from pathlib import Path
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import torch
-import nmrglue as ng
-from tqdm import tqdm
 import plotly.graph_objects as go
 
-def run(input_file, corrected_file, reference_file):
+def load_spectrum(path):
+    df = pd.read_csv(path, sep=None, engine="python", comment="#", header=None)
+    df = df.dropna(axis=1, how="all")  # usuń puste kolumny
+    freqs = df.iloc[:, 0].values.astype(float)
+    intensities = df.iloc[:, 1].values.astype(float)
+    return freqs, intensities
 
-    # ======= WCZYTANIE DANYCH =======
-    input_spectrum_file = os.getenv("INPUT_SPECTRUM_FILE", input_file)
-    corrected_spectrum_file = os.getenv("CORRECTED_SPECTRUM_FILE", corrected_file)
-    reference_spectrum_file = os.getenv("REFERENCE_SPECTRUM_FILE", reference_file)
 
-    def load_spectrum(path):
-        df = pd.read_csv(path, sep=None, engine="python", comment="#", header=None)
-        df = df.dropna(axis=1, how="all")  # usuń puste kolumny
-        freqs = df.iloc[:, 0].values.astype(float)
-        intensities = df.iloc[:, 1].values.astype(float)
-        return freqs, intensities
+def display_spectra(input_file, corrected_file1, reference_file=None, corrected_file2=None):
+    freqs_input, spec_input = load_spectrum(input_file)
+    freqs_corr1, spec_corr1 = load_spectrum(corrected_file1)
 
-    freqs_input, spec_input = load_spectrum(input_spectrum_file)
-    freqs_corr, spec_corr = load_spectrum(corrected_spectrum_file)
-    freqs_ref, spec_ref = load_spectrum(reference_spectrum_file)
+    plot_currect_file2 = corrected_file2 is not None
+    plot_reference_file = reference_file is not None
+    if plot_currect_file2:
+        freqs_corr2, spec_corr2 = load_spectrum(corrected_file2)
+    if plot_reference_file:
+        freqs_ref, spec_ref = load_spectrum(reference_file)
 
     # ======= NORMALIZACJA =======
     norm_val = spec_input.max()
     spec_input /= norm_val
-    spec_corr /= norm_val
-    spec_ref /= norm_val
+    spec_corr1 /= norm_val
+    if plot_currect_file2:
+        spec_corr2 /= norm_val
+    if plot_reference_file:
+        spec_ref /= norm_val
 
     # ======= WYKRES =======
     fig = go.Figure()
@@ -45,23 +41,34 @@ def run(input_file, corrected_file, reference_file):
         line=dict(color='#EF553B')
     ))
 
-    # Corrected spectrum
+    # Corrected spectrum1
     fig.add_trace(go.Scatter(
-        x=freqs_corr,
-        y=spec_corr,
+        x=freqs_corr1,
+        y=spec_corr1,
         mode='lines',
-        name='Corrected Spectrum',
+        name='Corrected Spectrum 1',
         line=dict(color='#00cc96')
     ))
 
+    # Corrected spectrum2
+    if plot_currect_file2:
+        fig.add_trace(go.Scatter(
+            x=freqs_corr2,
+            y=spec_corr2,
+            mode='lines',
+            name='Corrected Spectrum 2',
+            line=dict(color='#ff6700')
+        ))
+
     # Reference spectrum
-    fig.add_trace(go.Scatter(
-        x=freqs_ref,
-        y=spec_ref,
-        mode='lines',
-        name='Reference Spectrum',
-        line=dict(color='#636efa')
-    ))
+    if plot_reference_file:
+        fig.add_trace(go.Scatter(
+            x=freqs_ref,
+            y=spec_ref,
+            mode='lines',
+            name='Reference Spectrum',
+            line=dict(color='#636efa')
+        ))
 
     # ======= USTAWIENIA WYKRESU =======
     fig.update_layout(
@@ -85,7 +92,6 @@ def run(input_file, corrected_file, reference_file):
     fig.show()
     return
 
-
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
@@ -93,5 +99,5 @@ if __name__ == "__main__":
     parser.add_argument("--corrected", type=str, required=False)
     parser.add_argument("--reference", type=str, required=False)
     args = parser.parse_args()
-    run(args.input, args.corrected, args.reference)
+    display_spectra(args.input, args.corrected, args.reference)
 
