7fcb1aa585aa589bc53d44a9f95b2e4a180e8e33
Marek Bukowicki <m.bukowicki@uw.edu.pl>
2025-10-16 16:57:06 +0200

    add intesity scale and suffix parameters to predict scripts

--- predict.py
+++ predict.py
@@ -14,6 +14,9 @@ from shimnet.predict_utils import Defaults, resample_input_spectrum, resample_ou
 import warnings
 warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
 
+class Defaults:
+    SCALE = 16.0
+    SUFFIX = "_processed"
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -22,6 +25,8 @@ def parse_args():
     parser.add_argument("--weights", help="model weights")
     parser.add_argument("-o", "--output_dir", default=".", help="Output directory")
     parser.add_argument("--input_spectrometer_frequency", default=None, type=float, help="spectrometer frequency in MHz (input sample collection frequency). Empty if the same as in the training data")
+    parser.add_argument("--suffix", default=Defaults.SUFFIX, help=f"Output file suffix (default: {Defaults.SUFFIX})")
+    parser.add_argument("--intensity_scale", default=Defaults.SCALE, type=float, help=f"Intensity scaling factor (default: {Defaults.SCALE})")
     args = parser.parse_args()
     return args
     
@@ -52,7 +57,7 @@ if __name__ == "__main__":
         
         spectrum = torch.tensor(spectrum).float()
         # scale height of the spectrum
-        scaling_factor = Defaults.SCALE / spectrum.max()
+        scaling_factor = args.intensity_scale / spectrum.max()
         spectrum *= scaling_factor
 
         # correct spectrum
@@ -65,7 +70,7 @@ if __name__ == "__main__":
         output_prediction = resample_output_spectrum(input_freqs_model_ppm, freqs, prediction)
 
         # save result
-        output_file = output_dir / f"{Path(input_file).stem}_processed{Path(input_file).suffix}"
+        output_file = output_dir / f"{Path(input_file).stem}{args.suffix}{Path(input_file).suffix}"
 
         np.savetxt(output_file, np.column_stack((input_freqs_input_ppm, output_prediction)))
         print(f"saved to {output_file}")
